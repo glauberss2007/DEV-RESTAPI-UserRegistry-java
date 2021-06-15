5af9dcb8e4e20ef66f07bb8af9fb32e1b042e946
Glauber <glauberss2007@gmail.com>
2021-06-15 17:56:55 -0300

    Created PersonRepositry interface extanding JpaRepository

---
+++ src/main/java/com/dio/personapi/repositories/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.dio.personapi.repositories;
+
+import com.dio.personapi.entities.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
