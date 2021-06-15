3cf4926cfa91ead49a4f7132d4f4be9eb75c6818
Glauber <glauberss2007@gmail.com>
2021-06-15 20:12:29 -0300

    IMplemented ListAll method

--- src/main/java/com/dio/personapi/services/PersonService.java
+++ src/main/java/com/dio/personapi/services/PersonService.java
@@ -2,25 +2,37 @@ package com.dio.personapi.services;
 
 import com.dio.personapi.dto.MessageResponseDTO;
 import com.dio.personapi.entities.Person;
+import com.dio.personapi.dto.request.PersonDTO;
+import com.dio.personapi.mapper.PersonMapper;
 import com.dio.personapi.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
     private PersonRepository personRepository;
+
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository){
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person){
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO.builder()
                 .message("Created person with ID"+ savedPerson.getId())
                 .build();
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
     }
 }
