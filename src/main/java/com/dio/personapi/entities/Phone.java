8762bafcc34a385e277e1eee2880dc20474ad374
Glauber <glauberss2007@gmail.com>
2021-06-15 17:26:58 -0300

    Map variable of phone entity

--- src/main/java/com/dio/personapi/entities/Phone.java
+++ src/main/java/com/dio/personapi/entities/Phone.java
@@ -6,14 +6,34 @@ import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import com.dio.personapi.enums.PhoneType;
 
+import javax.persistence.*;
+
+//Auto Getters and Seters
 @Data
+
+//Clean object contstructor
 @Builder
+
+// Auto constructor insertion
 @AllArgsConstructor
 @NoArgsConstructor
 
+// Set this class as a an entity
+@Entity
+
 public class Phone {
+    // Autoincrement ID on DB related table
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    // Set type and data requirement
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private PhoneType type;
+
+    // Set data requirement
+    @Column(nullable = false)
     private String number;
 }
 
