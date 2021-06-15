42f7731ed86fa96afedb323fe2573a023a0e4e7b
Glauber <glauberss2007@gmail.com>
2021-06-15 20:11:09 -0300

    Created person Mapper interface

---
+++ src/main/java/com/dio/personapi/mapper/PersonMapper.java
@@ -0,0 +1,19 @@
+package com.dio.personapi.mapper;
+
+import com.dio.personapi.dto.request.PersonDTO;
+import com.dio.personapi.entities.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
